c80989fb16531ebcc2c08bd9a067a32b3ed89600
Benoit Moussaud <bmoussaud@xebialabs.com>
2016-04-27 17:58:29 +0200

    Fix #18: add displayStepLogs property

--- src/main/resources/xldeploy/deployTask.py
+++ src/main/resources/xldeploy/deployTask.py
@@ -37,7 +37,9 @@ taskId = xldClient.get_deployment_task_id(deployment)
 print "Execute task with id: %s" % taskId
 taskState = xldClient.invoke_task_and_wait_for_result(taskId, pollingInterval, numberOfPollingTrials, continueIfStepFails, numberOfContinueRetrials, failOnPause)
 
-xldClient.display_step_logs(taskId)
+if displayStepLogs:
+    print "Display the step logs"
+    xldClient.display_step_logs(taskId)
 
 if taskState in ('DONE', 'EXECUTED'):
     print "Deployment ended in %s \n" % taskState
