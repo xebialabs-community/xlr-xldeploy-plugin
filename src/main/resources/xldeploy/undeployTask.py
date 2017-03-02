2536d8a97e83f3a197955060c713ea750b570965
Joris De Winne <joris.dewinne@gmail.com>
2017-03-02 10:30:11 +0100

    Allow the undeployment task to not fail if the deployedApplication doesn't exist

--- src/main/resources/xldeploy/undeployTask.py
+++ src/main/resources/xldeploy/undeployTask.py
@@ -5,28 +5,35 @@
 #
 
 import sys
+
 from xldeploy.XLDeployClientUtil import XLDeployClientUtil
 
 xld_client = XLDeployClientUtil.create_xldeploy_client(xldeployServer, username, password)
 
 deployment = None
-deployment_package = xld_client.get_deployment_package("%s/%s" % (environment, deployedApplication))
+deployment_package = "{0}/{1}".format(environment, deployedApplication)
+print "deployment package is {0}".format(deployment_package)
 if xld_client.deployment_exists(deployment_package, environment):
     print "Undeploying [%s] from environment [%s] \n" % (deployment_package, environment)
-    deployment = xld_client.deployment_prepare_undeploy("%s/%s" % (environment, deployedApplication), orchestrators, deployedApplicationProperties)
+    deployment = xld_client.deployment_prepare_undeploy("%s/%s" % (environment, deployedApplication), orchestrators,
+                                                        deployedApplicationProperties)
 else:
-    print "No deployed application found [%s] for environment [%s] \n" % (deployedApplication, environment)
-    sys.exit(1)
+    if failIfApplicationDoesNotExist:
+        raise Exception("No deployed application found [%s] for environment [%s]" % (deployedApplication, environment))
+    else:
+        print "No deployed application found [%s] for environment [%s] \n" % (deployedApplication, environment)
+        sys.exit(0)
 
 print "Creating a deployment task \n"
 task_id = xld_client.get_deployment_task_id(deployment)
 
 print "Execute task with id: %s" % task_id
-task_state = xld_client.invoke_task_and_wait_for_result(task_id, pollingInterval, numberOfPollingTrials, continueIfStepFails, numberOfContinueRetrials, failOnPause)
+task_state = xld_client.invoke_task_and_wait_for_result(task_id, pollingInterval, numberOfPollingTrials,
+                                                        continueIfStepFails, numberOfContinueRetrials, failOnPause)
 
 xld_client.display_step_logs(task_id)
 
-if task_state in ('DONE','EXECUTED'):
+if task_state in ('DONE', 'EXECUTED'):
     print "Deployment ended in %s \n" % task_state
     xld_client.archive_task(task_id)
     sys.exit(0)
@@ -36,7 +43,8 @@ if rollbackOnError and task_state in ('FAILED', 'STOPPED'):
     print "Going to rollback \n"
     xld_client.stop_task(task_id)
     rollback_task_id = xld_client.deployment_rollback(task_id)
-    task_state = xld_client.invoke_task_and_wait_for_result(rollback_task_id, pollingInterval, numberOfPollingTrials, continueIfStepFails, numberOfContinueRetrials)
+    task_state = xld_client.invoke_task_and_wait_for_result(rollback_task_id, pollingInterval, numberOfPollingTrials,
+                                                            continueIfStepFails, numberOfContinueRetrials)
     xld_client.archive_task(rollback_task_id)
     sys.exit(1)
 elif task_state in ('FAILED', 'STOPPED'):
