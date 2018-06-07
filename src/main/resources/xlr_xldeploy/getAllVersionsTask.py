a94117f9a83d173f9013ff226ec0161a53a71439
BERTHIER Clement (IT-CE) <clement.berthier.prestataire@it-ce.fr>
2018-06-07 10:34:46 +0200

    Modify change for stripApplication to a one liner

--- src/main/resources/xlr_xldeploy/getAllVersionsTask.py
+++ src/main/resources/xlr_xldeploy/getAllVersionsTask.py
@@ -23,11 +23,7 @@ if throwOnFail and not response:
 packageIds = xld_client.get_all_package_version(applicationId)
 
 if stripApplications:
-    packageIdsTmp = list()
-    for version in packageIds:
-        version = version.partition('/')[2]
-        packageIdsTmp.append(version)
-    packageIds = packageIdsTmp
+    packageIds = [version.partition('/')[2] for version in packageIds]
 
 if throwOnFail and len(packageIds) == 0:
 	raise Exception(applicationId + " exists but has no versions")
